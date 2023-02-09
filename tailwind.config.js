c9a2233473090b7817db699ede76d10e2747f970
HadiUzzaman04 <hadi24x7@gmail.com>
2023-02-09 15:59:06 +0600

    all pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,45 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
+      xs: {'min': '320px', 'max': '480px'},
+      // => @media (min-width: 320px) { ... }
+
+      sm: {'min': '481px', 'max': '768px'},
+      // => @media (min-width: 640px) { ... }
+      md: {'min': '769px', 'max': '1024px'},  
+      // => @media (min-width: 768px) { ... }
+
+      lg: {'min': '1025px', 'max': '1440px'},
+      
+      // => @media (min-width: 1024px) { ... }
+      xl: {'min': '1441px','max': '2500px'},
+      // => @media (min-width: 1280px) { ... } 
+      xxl: {'min': '2500px','max': '2561px'},    
     },
-    extend: {},
+    extend: {
+      colors:{
+        'darkBlue': {
+          700: '#111827',
+          
+        },
+        'purple':{
+          100: '#445268'
+        },
+        'myGray':{
+          200: '#E5E3E3'
+        },
+        'myGreen':{
+          100: "#F3F6E7"
+        },
+        'myBlack':{
+          100: "#1A1A1A"
+        },
+      },
+      fontFamily:{
+         body:['Montserrat']
+      },
+       
+    },
   },
   plugins: [],
 }
